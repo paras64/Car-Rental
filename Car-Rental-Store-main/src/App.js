beada3e34a5fe0de52fc7fcd893e8e03fc02bf70
Kartik Gothwal <kartikgothwal28@gmail.com>
2023-10-23 01:28:57 +0530

    added the lazy loading

--- Car-Rental-Store-main/src/App.js
+++ Car-Rental-Store-main/src/App.js
@@ -1,31 +1,60 @@
 import "./App.css";
-import Home from "./component/Home";
-import About from "./component/About";
-import TestimonialComponent from "./component//TestimonialComponent";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
-import VehicalModels from "./component/VehicalModels";
-import Login from "./component/LoginRegistration/Login";
-import Registration from "./component/LoginRegistration/Registration";
-import ForgotPassword from "./component/LoginRegistration/ForgotPassword";
-import ConfirmPassword from "./component/LoginRegistration/ConfirmPassword";
-import Orders from "./component/Orders";
-import Contact from "./component/Contact";
-import NotFound from "./NotFound";
-import Ourteam from "./component/Ourteam";
-import AdminLogin from "./component/Admin/AdminLogin";
-import AdminDashboard from "./component/Admin/AdminDashboard";
-import AdminProvider from "./component/Admin/AdminProvider";
-import AddProducts from "./component/Admin/AddProducts";
-import UserProvider from "./component/LoginRegistration/UserProvider";
-import ModifyProduct from "./component/Admin/ModifyProduct";
-import ModifyFaq from "./component/Admin/ModifyFaq";
-import FaqProvider from "./component/Admin/FaqProvider";
-import AllUserProvider from "./component/Admin/AllUserProvider";
+import { lazy } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const VehicalModels = lazy(() => import("./component/VehicalModels"));
+const Login = lazy(() => import("./component/LoginRegistration/Login"));
+const Registration = lazy(() =>
+  import("./component/LoginRegistration/Registration")
+);
+const ForgotPassword = lazy(() =>
+  import("./component/LoginRegistration/ForgotPassword")
+);
+const ConfirmPassword = lazy(() =>
+  import("./component/LoginRegistration/ConfirmPassword")
+);
+const Orders = lazy(() => import("./component/Orders"));
+const Contact = lazy(() => import("./component/Contact"));
+const NotFound = lazy(() => import("./NotFound"));
+const Ourteam = lazy(() => import("./component/Ourteam"));
+const AdminLogin = lazy(() => import("./component/Admin/AdminLogin"));
+const AdminDashboard = lazy(() => import("./component/Admin/AdminDashboard"));
+const AdminProvider = lazy(() => import("./component/Admin/AdminProvider"));
+const AddProducts = lazy(() => import("./component/Admin/AddProducts"));
+const UserProvider = lazy(() =>
+  import("./component/LoginRegistration/UserProvider")
+);
+const ModifyProduct = lazy(() => import("./component/Admin/ModifyProduct"));
+const ModifyFaq = lazy(() => import("./component/Admin/ModifyFaq"));
+const FaqProvider = lazy(() => import("./component/Admin/FaqProvider"));
+const AllUserProvider = lazy(() => import("./component/Admin/AllUserProvider"));
+const Home = lazy(() => import("./component/Home"));
+const About = lazy(() => import("./component/About"));
+const TestimonialComponent = lazy(() =>
+  import("./component//TestimonialComponent")
+);
+
+// import Home from "./component/Home";
+// import About from "./component/About";
+// import TestimonialComponent from "./component//TestimonialComponent";
+// import VehicalModels from "./component/VehicalModels";
+// import Login from "./component/LoginRegistration/Login";
+// import Registration from "./component/LoginRegistration/Registration";
+// import ForgotPassword from "./component/LoginRegistration/ForgotPassword";
+// import ConfirmPassword from "./component/LoginRegistration/ConfirmPassword";
+// import Orders from "./component/Orders";
+// import Contact from "./component/Contact";
+// import NotFound from "./NotFound";
+// import Ourteam from "./component/Ourteam";
+// import AdminLogin from "./component/Admin/AdminLogin";
+// import AdminDashboard from "./component/Admin/AdminDashboard";
+// import AdminProvider from "./component/Admin/AdminProvider";
+// import AddProducts from "./component/Admin/AddProducts";
+// import UserProvider from "./component/LoginRegistration/UserProvider";
+// import ModifyProduct from "./component/Admin/ModifyProduct";
+// import ModifyFaq from "./component/Admin/ModifyFaq";
+// import FaqProvider from "./component/Admin/FaqProvider";
+// import AllUserProvider from "./component/Admin/AllUserProvider";
 function App() {
   return (
     <>
