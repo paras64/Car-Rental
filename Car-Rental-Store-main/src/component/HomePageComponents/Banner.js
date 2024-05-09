4a8954953edd5b0bfc864fe7053540e67148fbac
paras64 <146513526+paras64@users.noreply.github.com>
2024-05-09 03:11:14 +0000

    new changes

--- Car-Rental-Store-main/src/component/HomePageComponents/Banner.js
+++ Car-Rental-Store-main/src/component/HomePageComponents/Banner.js
@@ -197,7 +197,7 @@ export default function Banner() {
         </section>
         <section className="google-maps-images" id="maps">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117756.85248567844!2d75.65771979726566!3d22.731897000000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962fdc859bc94ab%3A0xdb2d9bb53213f853!2sCar%20Rental!5e0!3m2!1sen!2sin!4v1685815339461!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.2507913563077!2d77.81466497510775!3d23.523480578825883!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397c046104f25955%3A0xa3dd1411270920fe!2sSherpura%20Rd%2C%20Sherpura%2C%20Vidisha%2C%20Madhya%20Pradesh%20464001!5e0!3m2!1sen!2sin!4v1715171249252!5m2!1sen!2sin"
             width="100%"
             height="450"
             style={{ border: "0" }}
